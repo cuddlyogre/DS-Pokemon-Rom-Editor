b5064eded6e044119d8c9f4084db8095b24f2ad2
Matthew Morrison <cuddlyogre@gmail.com>
2022-12-28 20:01:44 -0600

    MatrixNavigator

---
+++ DS_Map/Editors/MatrixNavigator.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace DSPRE.Editors
+{
+  partial class MatrixNavigator
+  {
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.eventMatrixPanel = new System.Windows.Forms.Panel();
+      this.eventMatrixPictureBox = new System.Windows.Forms.PictureBox();
+      this.matrixNavigatorLabel = new System.Windows.Forms.Label();
+      this.eventMatrixPanel.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.eventMatrixPictureBox)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // eventMatrixPanel
+      // 
+      this.eventMatrixPanel.AutoScroll = true;
+      this.eventMatrixPanel.BackColor = System.Drawing.SystemColors.Menu;
+      this.eventMatrixPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+      this.eventMatrixPanel.Controls.Add(this.eventMatrixPictureBox);
+      this.eventMatrixPanel.Location = new System.Drawing.Point(3, 15);
+      this.eventMatrixPanel.Name = "eventMatrixPanel";
+      this.eventMatrixPanel.Size = new System.Drawing.Size(448, 149);
+      this.eventMatrixPanel.TabIndex = 47;
+      // 
+      // eventMatrixPictureBox
+      // 
+      this.eventMatrixPictureBox.Location = new System.Drawing.Point(0, -1);
+      this.eventMatrixPictureBox.Name = "eventMatrixPictureBox";
+      this.eventMatrixPictureBox.Size = new System.Drawing.Size(440, 150);
+      this.eventMatrixPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+      this.eventMatrixPictureBox.TabIndex = 46;
+      this.eventMatrixPictureBox.TabStop = false;
+      this.eventMatrixPictureBox.Click += new System.EventHandler(this.eventMatrixPictureBox_Click);
+      // 
+      // matrixNavigatorLabel
+      // 
+      this.matrixNavigatorLabel.AutoSize = true;
+      this.matrixNavigatorLabel.Location = new System.Drawing.Point(2, 1);
+      this.matrixNavigatorLabel.Name = "matrixNavigatorLabel";
+      this.matrixNavigatorLabel.Size = new System.Drawing.Size(84, 13);
+      this.matrixNavigatorLabel.TabIndex = 48;
+      this.matrixNavigatorLabel.Text = "Matrix Navigator";
+      // 
+      // MatrixNavigator
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.eventMatrixPanel);
+      this.Controls.Add(this.matrixNavigatorLabel);
+      this.Name = "MatrixNavigator";
+      this.Size = new System.Drawing.Size(454, 166);
+      this.eventMatrixPanel.ResumeLayout(false);
+      this.eventMatrixPanel.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.eventMatrixPictureBox)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Panel eventMatrixPanel;
+    private System.Windows.Forms.PictureBox eventMatrixPictureBox;
+    private System.Windows.Forms.Label matrixNavigatorLabel;
+  }
+}
---
+++ DS_Map/Editors/MatrixNavigator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DSPRE.Editors {
+  public partial class MatrixNavigator : UserControl {
+    public const byte tileSize = 16;
+    public int width;
+    public int height;
+    public List<int[]> positions;
+
+    public event EventHandler<CellMarkedEventArgs> CellMarked;
+
+    public MatrixNavigator() {
+      InitializeComponent();
+    }
+
+    private void eventMatrixPictureBox_Click(object sender, EventArgs e) {
+      Point coordinates = eventMatrixPictureBox.PointToClient(Cursor.Position);
+      Point mouseTilePos = new Point(coordinates.X / tileSize, coordinates.Y / tileSize);
+      MarkActiveCell(mouseTilePos.X, mouseTilePos.Y);
+    }
+
+    public void MarkActiveCell(int X, int Y) {
+      const int padding = 1;
+      eventMatrixPictureBox.Image = new Bitmap(padding + tileSize * width, padding + tileSize * height);
+
+      using (Graphics g = Graphics.FromImage(eventMatrixPictureBox.Image)) {
+        DrawEventMatrix(g, width, height);
+        MarkUsedCells(g, positions);
+        MarkChosenCell(g, X, Y);
+      }
+
+      /* Update PictureBox and current coordinates labels */
+      eventMatrixPictureBox.Invalidate();
+
+      CellMarked?.Invoke(this, new CellMarkedEventArgs() { X = X, Y = Y });
+    }
+
+    private void DrawEventMatrix(Graphics g, int width, int height) {
+      g.Clear(Color.Black);
+
+      Pen pen = Pens.White;
+      for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+          drawCell(g, pen, x, y);
+        }
+      }
+    }
+
+    private void MarkUsedCells(Graphics g, List<int[]> positions) {
+      Brush brush = Brushes.Orange;
+      for (int i = 0; i < positions.Count; i++) {
+        fillCell(g, brush, positions[i][0], positions[i][1]);
+      }
+    }
+
+    private void MarkChosenCell(Graphics g, int x, int y) {
+      Brush brush = Brushes.Lime;
+      fillCell(g, brush, x, y);
+    }
+
+    private void drawCell(Graphics g, Pen pen, int x, int y) {
+      const int padding = 1;
+      const int cellSize = 14;
+      Rectangle eventMatrixRectangle = new Rectangle(padding + tileSize * x, padding + tileSize * y, cellSize, cellSize);
+      g.DrawRectangle(pen, eventMatrixRectangle);
+    }
+
+    private void fillCell(Graphics g, Brush brush, int x, int y) {
+      const int padding = 2;
+      const int cellSize = 13;
+      Rectangle eventMatrixRectangle = new Rectangle(padding + tileSize * x, padding + tileSize * y, cellSize, cellSize);
+      g.FillRectangle(brush, eventMatrixRectangle);
+    }
+  }
+  public class CellMarkedEventArgs : EventArgs {
+    public int X { get; set; }
+    public int Y { get; set; }
+  }
+}
